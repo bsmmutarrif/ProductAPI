04532636e136593165152cbca51ad7feb4afdc9d
bsmmutarrif <mutarrif.nbr@gmail.com>
2024-11-14 16:53:35 +0600

    unit test first push

--- src/main/java/com/example/productapi/repository/ProductRepository.java
+++ src/main/java/com/example/productapi/repository/ProductRepository.java
@@ -1,6 +1,12 @@
 package com.example.productapi.repository;
 import com.example.productapi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product,
 Long> {
+
+
+
 }
